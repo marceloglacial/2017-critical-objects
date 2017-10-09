da4f61fd85adbd0e461cff5bc8141504cca40771
Marcelo Glacial <glacial@gmail.com>
2017-10-09 19:34:20 -0400

    Mute video

--- dist/js/master.js
+++ dist/js/master.js
@@ -37,7 +37,7 @@ $('.count-box').each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     }, {
-        duration: 5000,
+        duration: 10000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
