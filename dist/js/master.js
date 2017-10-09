15772049f69ec5b35c74460bb58134a7792fa8a4
Marcelo Glacial <glacial@gmail.com>
2017-10-09 18:46:56 -0400

    Add animated counters

--- dist/js/master.js
+++ dist/js/master.js
@@ -31,3 +31,16 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+// Counter - https://codepen.io/shivasurya/pen/FatiB
+$('.count-box').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
